dfb73dbe08d669eab7b3f027f25474d0e298da3d
Serhey Doroshenko <prigovors@gmail.com>
2021-01-07 15:46:58 +0200

    Initial commit

---
+++ src/main/java/com/doroshenko/serhey/repository/ConfigServerRepositoryApplication.java
@@ -0,0 +1,7 @@
+package com.doroshenko.serhey.repository;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerRepositoryApplication {
+}
